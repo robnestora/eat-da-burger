56f60d548bab6e43f8e9fb7376b95b5ab2757b8c
Rob Nestora <30611018+robnestora@users.noreply.github.com>
2018-08-21 18:05:57 -0400

    trying to make a connection to jaws

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,8 +9,8 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+  }
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +19,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
